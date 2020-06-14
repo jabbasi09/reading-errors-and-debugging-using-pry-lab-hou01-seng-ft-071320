61eebdce86671525372ee1bb0a731088a8bf55b2
Junaid Abbasi <juni.abbasi@icloud.com>
2020-06-14 07:54:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,8 +12,10 @@ end
 
 def selection(num)
   if num = 1
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
+    binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
 end
@@ -21,5 +23,4 @@ end
 def runner
   prompt_user
   selection(get_user_input)
-  binding.pry
 end
