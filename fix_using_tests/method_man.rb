30314820be4a4105c11a59a1cb5df175e0b8d36d
Junaid Abbasi <juni.abbasi@icloud.com>
2020-06-14 08:07:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,10 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def start_game(player1, player2)
+  binding.pry
   puts "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player2)
   puts "#{player1} is better than #{player2}"
 end
-binding.pry
